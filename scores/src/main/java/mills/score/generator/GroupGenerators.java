9f407fa35fe5c96f764302f09fc0d22a53e0b5f9
ds <stueken@t-online.de>
2019-12-30 13:10:56 +0100

    SlicesGroups

--- scores/src/main/java/mills/score/generator/GroupGenerators.java
+++ scores/src/main/java/mills/score/generator/GroupGenerators.java
@@ -1,13 +1,8 @@
 package mills.score.generator;
 
-import mills.bits.Clops;
 import mills.bits.Player;
-import mills.bits.PopCount;
 import mills.index.IndexProcessor;
-import mills.position.Positions;
 import mills.score.Score;
-import mills.stones.Mover;
-import mills.stones.Stones;
 
 import java.util.List;
 import java.util.concurrent.ForkJoinTask;
@@ -48,9 +43,6 @@ public class GroupGenerators {
             //ForkJoinTask.invokeAll(tasks);
         }
 
-        Long i201 = self.moved.group.get(Clops.of(self.moved.pop(), PopCount.EMPTY)).index().i201(0);
-        MovedPosition pos = movedPosition(i201, Player.White);
-
         return tasks.size();
     }
 
@@ -63,57 +55,5 @@ public class GroupGenerators {
                 slice.processScores(processor, score);
             }
         };
-    }
-
-    public MovedPosition movedPosition(long i201, Player player) {
-        boolean inverted = player!=this.player();
-        long j201 = inverted ? Positions.inverted(i201) : i201;
-        Clops clops = Positions.clops(j201);
-        ScoreSet scores = self.moved.group.get(clops).scores;
-        return new MovedPosition(scores, i201, player, null);
-    }
-
-    public ScoredPosition closedPosition(long i201, Player player) {
-        return self.closed.position(i201, player);
-    }
-
-    public class MovedPosition extends ScoredPosition {
-
-        final List<MovedPosition> moved;
-        final List<ScoredPosition> closed;
-
-        @Override
-        protected MovedPosition position(long i201, Player player, ScoredPosition inverted) {
-            return new MovedPosition(scores, i201, player, inverted);
-        }
-
-        public MovedPosition(ScoreSet scores, long i201, Player player, ScoredPosition inverted) {
-            super(scores, i201, player, inverted);
-
-            moved = moved();
-            closed = closed();
-        }
-
-        List<MovedPosition> moved() {
-            Mover mover = scores.mover(player.other());
-
-            int stay = Stones.stones(i201, player.other());
-            int move = Stones.stones(i201, player);
-            int closed = Stones.closed(move);
-            mover.move(stay, move, move^closed);
-
-            return mover.transform(i201->movedPosition(i201, player));
-        }
-
-        List<ScoredPosition> closed() {
-            Mover mover = scores.mover(player.other());
-
-            int stay = Stones.stones(i201, player.other());
-            int move = Stones.stones(i201, player);
-            int closed = Stones.closed(move);
-            mover.move(stay, move, closed);
-
-            return mover.transform(i201->closedPosition(i201, player));
-        }
     }
 }
---
+++ scores/src/test/java/mills/score/generator/MovedGroupTest.java
@@ -0,0 +1,98 @@
+package mills.score.generator;
+
+import mills.bits.Player;
+import mills.bits.PopCount;
+import mills.index.IndexProvider;
+import mills.index.PosIndex;
+import mills.score.Score;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+public class MovedGroupTest {
+    static final IndexProvider indexes = IndexProvider.load();
+
+    PopCount p33 = PopCount.of(3,3);
+
+    PopCount c00 = PopCount.of(0,0);
+    PopCount c10 = PopCount.of(1,0);
+    PopCount c01 = PopCount.of(0,1);
+    PopCount c11 = PopCount.of(1,1);
+
+    PosIndex i33c00 = indexes.build(p33, c00);
+    PosIndex i33c01 = indexes.build(p33, c01);
+    PosIndex i33c10 = indexes.build(p33, c10);
+    PosIndex i33c11 = indexes.build(p33, c11);
+
+    Player b = Player.Black;
+    Player w = Player.White;
+
+    @Test
+    public void process() {
+
+    }
+
+    ClosedGroup closed() {
+
+        List<Slices<ScoreSlice>> slices = List.of(
+                closed(i33c10, w),
+                closed(i33c11, w));
+
+        return new ClosedGroup(p33, w, slices);
+    }
+
+    Slices<ScoreSlice> closed(PosIndex index, Player player) {
+
+        ScoreSet scores = new ScoreSet(index, player) {
+
+            @Override
+            public String toString() {
+                return String.format("lost(%s%c%s)", pop(), player.key(), clop());
+            }
+
+            @Override
+            public int getScore(int index) {
+                return Score.LOST.value;
+            }
+
+            @Override
+            ScoreSlice openSlice(int index) {
+                ScoreSlice slice = super.openSlice(index);
+                slice.dirty[Score.LOST.value] = -1;
+                slice.max=Score.LOST.value;
+                return slice;
+            }
+        };
+
+        return Slices.generate(scores, scores::openSlice);
+    }
+
+    MovedGroup target() {
+
+        List<Slices<MapSlice>> slices = List.of(
+            target(i33c00, w),
+            target(i33c01, w),
+            target(i33c10, w),
+            target(i33c11, w));
+
+        return new MovedGroup(p33, w, closed(), slices);
+    }
+
+    Slices<MapSlice> target(PosIndex index, Player player) {
+        ByteBuffer buffer = ByteBuffer.allocateDirect(index.range());
+        ScoreMap scores = new ScoreMap(index, player, buffer);
+        //{
+        //@Override
+        //MapSlice openSlice(int index) {
+        //    MapSlice slice = super.openSlice(index);
+        //    slice.process((posIndex, i201) -> {
+        //        int n = slice.unresolved(i201);
+        //        if(n==0)
+        //            slice.setScore(slice.offset(posIndex), Score.LOST.value);
+        //    });
+        //    return slice;
+        //};
+        return Slices.generate(scores, scores::openSlice);
+    }
+}
